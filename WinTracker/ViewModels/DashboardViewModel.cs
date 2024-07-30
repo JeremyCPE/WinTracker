2e3e0b8ba9c8b1c76d1fcee78dc1414e3dec58ee
JeremyCPE <mohariry.jeremy@gmail.com>
2024-07-30 18:21:28 +0300

    (feat) PieChart

--- WinTracker/ViewModels/DashboardViewModel.cs
+++ WinTracker/ViewModels/DashboardViewModel.cs
@@ -1,17 +1,22 @@
 ﻿using LiveChartsCore;
-using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Extensions;
+using LiveChartsCore.SkiaSharpView.Painting;
+using LiveChartsCore.SkiaSharpView.VisualElements;
+using SkiaSharp;
 
 namespace WinTracker.ViewModels
 {
     public class DashboardViewModel
     {
-        public ISeries[] Series { get; set; } = new ISeries[]
-        {
-            new LineSeries<double>
+        public IEnumerable<ISeries> Series { get; set; } = new[] { 2, 4, 1, 4, 3 }.AsPieSeries();
+
+        public LabelVisual Title { get; set; } =
+            new LabelVisual
             {
-                Values = new double[] {2,1,3,5,3,4,6},
-                Fill = null
-            }
-        };
+                Text = "Time Used",
+                TextSize = 25,
+                Padding = new LiveChartsCore.Drawing.Padding(15),
+                Paint = new SolidColorPaint(SKColors.DarkSlateGray)
+            };
     }
 }
