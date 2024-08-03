f94ef119547f590163f374ec4daa4c637904115a
JeremyCPE <mohariry.jeremy@gmail.com>
2024-08-03 17:57:25 +0300

    (feat) include TrackActiveWindow in  TrackingService

--- WinTracker/Services/TrackingServices.cs
+++ WinTracker/Services/TrackingServices.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows.Threading;
+using WinTracker.Communication;
 using WinTracker.Database;
 using WinTracker.Models;
 
@@ -10,6 +13,12 @@ namespace WinTracker.Utils
         /// Cache applicationInfo
         /// </summary>
         public static ObservableCollection<ApplicationInfo> _applicationInfos;
+
+        public static Guid _lastUsedApp;
+
+
+        public static List<int> NotReadableList { get; set; } = [];
+
 
         public static List<ApplicationInfo> Load()
         {
@@ -23,6 +32,72 @@ namespace WinTracker.Utils
 
         }
 
+        /// <summary>
+        /// Track active programs on Windows, ignore those where we don't have any info
+        /// </summary>
+        public static void TrackActiveWindow(Dispatcher _dispatcher)
+        {
+            while (true)
+            {
+                try
+                {
+                    Process? process = ProcessUtils.GetForegroundProcess();
+                    if (IsNullOrIgnorable(process))
+                    {
+                        continue;
+                    }
+
+                    ApplicationInfo? appInfo = ApplicationInfo.ConvertFromProcess(process);
+                    if (appInfo is null)
+                    {
+                        if (!NotReadableList.Contains(process.Id))
+                        {
+                            NotReadableList.Add(process.Id);
+                        }
+                        continue;
+                    }
+                    ApplicationInfo? usedAppInfo = _applicationInfos.FirstOrDefault(d => d.ProcessInfo.ProcessName == appInfo.ProcessInfo.ProcessName);
+                    _dispatcher.Invoke(() =>
+                    {
+                        if (usedAppInfo is null)
+                        {
+                            Debug.WriteLine("[INFO] New app found !");
+                            _lastUsedApp = appInfo.Guid;
+                            _dispatcher.Invoke(() =>
+                            {
+                                _applicationInfos.Add(appInfo);
+                            });
+                        }
+                        else
+                        {
+                            Debug.WriteLine("[DEBUG] Update of a known app!");
+                            _lastUsedApp = usedAppInfo.Guid;
+                            usedAppInfo.Update();
+                        }
+                        UpdateStatusOfUnusedApp();
+                    });
+
+                    JsonDatabase.Save(ApplicationInfo.ToDtoList(_applicationInfos.ToList()));
+
+                    Thread.Sleep(1000);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"[ERR] An exception has been thrown, {ex.ToString}");
+                }
+
+            }
+        }
+
+        private static bool IsNullOrIgnorable(Process? process)
+        {
+            return process is null || NotReadableList.Contains(process.Id);
+        }
+
+        private static void UpdateStatusOfUnusedApp()
+        {
+            _applicationInfos.Where(d => d.Guid != _lastUsedApp).ToList().ForEach(a => a.Stop());
+        }
 
     }
 }
--- WinTracker/ViewModels/MainWindowViewModel.cs
+++ WinTracker/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.Windows.Threading;
-using WinTracker.Communication;
 using WinTracker.Database;
 using WinTracker.Models;
 using WinTracker.Utils;
@@ -28,13 +26,8 @@ namespace WinTracker.ViewModels
             }
         }
 
-        private Guid _lastUsedApp;
-
-
-
         private readonly Dispatcher _dispatcher;
 
-        public List<int> NotReadableList { get; set; } = [];
 
         public ObservableCollection<ApplicationInfo> ApplicationInfos
         {
@@ -57,7 +50,7 @@ namespace WinTracker.ViewModels
             NavbarViewModel = new NavbarViewModel(this);
             CurrentView = new Home();
 
-            JsonDatabase.DeleteOldFile(); // delete oldfile on the folder
+            JsonDatabase.DeleteOldFile();
 
             List<ApplicationInfo> appInfos = TrackingServices.Load();
             ApplicationInfos = new ObservableCollection<ApplicationInfo>(appInfos);
@@ -69,7 +62,7 @@ namespace WinTracker.ViewModels
 
         private void StartTracking()
         {
-            Thread activeWindowThread = new(new ThreadStart(TrackActiveWindow));
+            Thread activeWindowThread = new(() => TrackingServices.TrackActiveWindow(_dispatcher));
             activeWindowThread.Start();
         }
 
@@ -82,75 +75,6 @@ namespace WinTracker.ViewModels
         {
             CurrentView = new Dashboard();
         }
-
-
-        /// <summary>
-        /// TODO : Move to tracking services
-        /// </summary>
-        private void TrackActiveWindow()
-        {
-            while (true)
-            {
-                try
-                {
-                    Process? process = ProcessUtils.GetForegroundProcess();
-                    if (IsNullOrIgnorable(process))
-                    {
-                        continue;
-                    }
-
-                    ApplicationInfo? appInfo = ApplicationInfo.ConvertFromProcess(process);
-                    if (appInfo is null)
-                    {
-                        if (!NotReadableList.Contains(process.Id))
-                        {
-                            NotReadableList.Add(process.Id);
-                        }
-                        continue;
-                    }
-                    ApplicationInfo? usedAppInfo = ApplicationInfos.FirstOrDefault(d => d.ProcessInfo.ProcessName == appInfo.ProcessInfo.ProcessName);
-                    _dispatcher.Invoke(() =>
-                    {
-                        if (usedAppInfo is null)
-                        {
-                            Debug.WriteLine("[INFO] New app found !");
-                            _lastUsedApp = appInfo.Guid;
-                            _dispatcher.Invoke(() =>
-                            {
-                                ApplicationInfos.Add(appInfo);
-                            });
-                        }
-                        else
-                        {
-                            Debug.WriteLine("[DEBUG] Update of a known app!");
-                            _lastUsedApp = usedAppInfo.Guid;
-                            usedAppInfo.Update();
-                        }
-                        UpdateStatusOfUnusedApp();
-                    });
-
-                    JsonDatabase.Save(ApplicationInfo.ToDtoList(ApplicationInfos.ToList()));
-
-                    Thread.Sleep(1000);
-                }
-                catch (Exception ex)
-                {
-                    Debug.WriteLine($"[ERR] An exception has been thrown, {ex.ToString}");
-                }
-
-            }
-        }
-
-        private bool IsNullOrIgnorable(Process? process)
-        {
-            return process is null || NotReadableList.Contains(process.Id);
-        }
-
-        private void UpdateStatusOfUnusedApp()
-        {
-            ApplicationInfos.Where(d => d.Guid != _lastUsedApp).ToList().ForEach(a => a.Stop());
-        }
-
         protected void NotifyPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
