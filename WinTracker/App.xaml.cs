aff29b4d6fac4ae150019f695161805412929f52
JeremyCPE <mohariry.jeremy@gmail.com>
2024-08-30 00:28:43 +0800

    (feat) add Dependancy Injection

--- WinTracker/App.xaml.cs
+++ WinTracker/App.xaml.cs
@@ -1,12 +1,105 @@
-﻿using System.Windows;
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
 
-namespace WinTracker
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using System.Windows;
+using System.Windows.Threading;
+using WinTracker.Models;
+using WinTracker.Services;
+using Wpf.Ui;
+
+namespace WinTracker;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App
 {
+    // The.NET Generic Host provides dependency injection, configuration, logging, and other services.
+    // https://docs.microsoft.com/dotnet/core/extensions/generic-host
+    // https://docs.microsoft.com/dotnet/core/extensions/dependency-injection
+    // https://docs.microsoft.com/dotnet/core/extensions/configuration
+    // https://docs.microsoft.com/dotnet/core/extensions/logging
+    private static readonly IHost _host = Host.CreateDefaultBuilder()
+        .ConfigureAppConfiguration(c =>
+        {
+            string basePath =
+                Path.GetDirectoryName(AppContext.BaseDirectory)
+                ?? throw new DirectoryNotFoundException(
+                    "Unable to find the base directory of the application."
+                );
+            _ = c.SetBasePath(basePath);
+        })
+        .ConfigureServices(
+            (context, services) =>
+            {
+                // App Host
+                _ = services.AddHostedService<ApplicationHostService>();
+
+                // Theme manipulation
+                _ = services.AddSingleton<IThemeService, ThemeService>();
+
+                // TaskBar manipulation
+                _ = services.AddSingleton<ITaskBarService, TaskBarService>();
+
+                // Service containing navigation, same as INavigationWindow... but without window
+                _ = services.AddSingleton<INavigationService, NavigationService>();
+
+                // Main window with navigation
+                _ = services.AddSingleton<INavigationWindow, Views.MainWindow>();
+                _ = services.AddSingleton<ViewModels.MainWindowViewModel>();
+
+                // Views and ViewModels
+                _ = services.AddSingleton<Views.Dashboard>();
+                _ = services.AddSingleton<ViewModels.DashboardViewModel>();
+                _ = services.AddSingleton<Views.Home>();
+                _ = services.AddSingleton<ViewModels.HomeViewModel>();
+                _ = services.AddSingleton<Views.Settings>();
+                _ = services.AddSingleton<ViewModels.SettingsViewModel>();
+
+                // Configuration
+                _ = services.Configure<AppConfig>(context.Configuration.GetSection(nameof(AppConfig)));
+            }
+        )
+        .Build();
+
     /// <summary>
-    /// Interaction logic for App.xaml
+    /// Gets services.
     /// </summary>
-    public partial class App : Application
+    public static IServiceProvider Services
     {
+        get { return _host.Services; }
     }
 
+    /// <summary>
+    /// Occurs when the application is loading.
+    /// </summary>
+    private async void OnStartup(object sender, StartupEventArgs e)
+    {
+        await _host.StartAsync();
+    }
+
+    /// <summary>
+    /// Occurs when the application is closing.
+    /// </summary>
+    private async void OnExit(object sender, ExitEventArgs e)
+    {
+        await _host.StopAsync();
+
+        _host.Dispose();
+    }
+
+    /// <summary>
+    /// Occurs when an exception is thrown by an application but not handled.
+    /// </summary>
+    private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        // For more info see https://docs.microsoft.com/en-us/dotnet/api/system.windows.application.dispatcherunhandledexception?view=windowsdesktop-6.0
+    }
 }
---
+++ WinTracker/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Wpf.Ui.Demo.Mvvm.ViewModels;
+
+namespace WinTracker.ViewModels
+{
+    public partial class HomeViewModel : ViewModel
+    {
+
+        // Todo to fill
+    }
+}
