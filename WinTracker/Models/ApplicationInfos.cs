2f322ca932d3819b7317f4c2d7692b545e1820e1
JeremyCPE <mohariry.jeremy@gmail.com>
2024-09-07 17:41:03 +0800

    (feat) remove useless INotifiyPropertyChange

--- WinTracker/Models/ApplicationInfos.cs
+++ WinTracker/Models/ApplicationInfos.cs
@@ -1,38 +1,13 @@
 ﻿using System.Collections.ObjectModel;
-using System.ComponentModel;
 
 namespace WinTracker.Models
 {
-    public class ApplicationInfos : INotifyPropertyChanged
+    public class ApplicationInfos
     {
         /// <summary>
         /// Cache applicationInfo
         /// </summary>
         /// 
-        public ObservableCollection<ApplicationInfo> _list;
-        public ObservableCollection<ApplicationInfo> List
-        {
-            get
-            {
-                return _list;
-            }
-            set
-            {
-                _list = value;
-                OnPropertyChanged(nameof(List));
-            }
-        }
-
-        public ApplicationInfos()
-        {
-            _list = new ObservableCollection<ApplicationInfo>();
-        }
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        protected void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        public ObservableCollection<ApplicationInfo> List { get; set; } = new ObservableCollection<ApplicationInfo>();
     }
 }
